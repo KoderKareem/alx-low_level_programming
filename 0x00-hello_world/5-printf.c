1e85891a8199d4df1ceefb8244fdb77610695ce1
Abdulkarim <abdulkarimsunusi2001@gmail.com>
2023-03-16 17:51:41 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print a lin of code using print
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
