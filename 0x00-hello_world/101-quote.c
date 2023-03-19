88b37c48f456e75c9c94697d5778c7b635d59c7b
Abdulkarim <abdulkarimsunusi2001@gmail.com>
2023-03-19 13:38:25 -0700

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+{
+	wrtie(2, 
