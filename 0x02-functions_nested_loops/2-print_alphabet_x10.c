aa36fc9f4559d6c37451c0768904f32a41d9ab7e
Abdulkarim <abdulkarimsunusi2001@gmail.com>
2023-03-22 16:14:17 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,11 @@ include "main.h"
 void print_alphabet_x10(void)
 {
 	char letter;
-	char alphabet;
+	int count = 0;
+	while (count <= 9)
 
-	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		for (alphabet = 0; alphabet <= 10; alphabet++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 		_putchat('\n');
 	}
