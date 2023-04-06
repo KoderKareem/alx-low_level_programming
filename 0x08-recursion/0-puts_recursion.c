37db7a6cd3f2a96c22af2e544c755550a0c6e211
Abdulkarim <abdulkarimsunusi2001@gmail.com>
2023-04-06 06:23:50 -0700

    1-print_rev_recursio

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include main.h
+/*
+* main - this program will print a string
+*
+* Return: 0;
+*/
+void _puts_recursion(char *s){
+	if (*s)
+{
+	_puchar(*s);
+	_puts_recursion(s + 1);
+}
+else {
+	_putchar('\n');
+}
+}
