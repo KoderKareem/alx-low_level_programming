1f1e44b900c5e9beafa556cf732845e4ebef68b9
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-02 02:47:00 -0700

    I just finushed my first task on recurtion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * _puts_recursion- print recursion
+ *
+ * main- this program will pring recurtion
+ * *s- i pointer to a char
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');  /* Print a new line when the end of string is reached */
+		return;
+	}
+
+	putchar(*s);  /* Print the current */
+
+	_puts_recursion(s + 1);
+}
+int main(void)
+{
+
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
+
