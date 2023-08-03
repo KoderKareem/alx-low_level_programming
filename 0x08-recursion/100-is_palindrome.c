cd7699b8907f98e46d93c21fd0517c9e8f7edb94
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-03 15:44:21 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,9 +9,9 @@
 
 int is_palindrome(char *s)
 {
-	int length = strlen(s)l
+	int length = strlen(s);
 		int i, j;
-	for (i = 0; j = length - 1; i < j; i++; j--)
+	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
 		if (s[i] != s[j])
 		{
