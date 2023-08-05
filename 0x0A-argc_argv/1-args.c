d713db42845f086793622c583716e00be5091956
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-05 15:18:23 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
-/**
+/* main: this program will prints the number of arguments passed into it.
  *
+ * @argc: Thsi will print the number of argument
+ * @argv: This will print the name of argument passed into it.
  *
- *
- *
- *
+ * Return (0);
  */
 
 int main(int argc, char *argv[])
-{
-	for(int i = 0; i < argc; i++)
-	{
-		printf("%d\n",i);
-	}
-	return (0);
+{	
+		printf("%d", argc - 1);
+
+	return 0;
 }
