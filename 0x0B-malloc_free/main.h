0a0c4ee7314de68919786b79fa8b2279e3a57015
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-09 15:11:20 -0700

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,17 +10,5 @@ char **strtow(char *str);
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 #endif
