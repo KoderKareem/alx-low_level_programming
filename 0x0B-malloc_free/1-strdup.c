09bb9323095928a5ab6095fdab375cdd8923aa43
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-10 16:39:34 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,39 +15,21 @@ char *_strdup(char *str)
 
     char *dup_str;
     int length = 0;
+    int i;
 
-    // Calculate the length of the input string
+    /* Calculate the length of the input string */
     while (str[length] != '\0')
         length++;
 
-    // Allocate memory for the duplicated string (+1 for the null-terminator)
-    dup_str = malloc((length + 1) * sizeof(char));
+    /* Allocate memory for the duplicated string (+1 for the null-terminator) */
+    dup_str = (char *)malloc((length + 1) * sizeof(char));
 
     if (dup_str == NULL)
         return NULL;
 
-    // Copy the content of the input string to the duplicated string
-    for (int i = 0; i <= length; i++)
+    /* Copy the content of the input string to the duplicated string */
+    for (i = 0; i <= length; i++)
         dup_str[i] = str[i];
 
     return dup_str;
-}
-
-int main(void)
-{
-    char *original = "Hello, world!";
-    char *duplicate = _strdup(original);
-
-    if (duplicate != NULL)
-    {
-        printf("Original: %s\n", original);
-        printf("Duplicate: %s\n", duplicate);
-        free(duplicate);
-    }
-    else
-    {
-        printf("Memory allocation failed.\n");
-    }
-
-    return 0;
 }
