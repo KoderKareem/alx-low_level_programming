c23f79ff1727cc26a82f9cfa4a2ab2997053000a
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-11 16:25:42 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,13 +10,13 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
+	void *ptr = malloc(b);
 
-    if (ptr == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(98);
-    }
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Memory allocation failed\n");
+		exit(98);
+	}
 
-    return ptr;
+	return (ptr);
 }
