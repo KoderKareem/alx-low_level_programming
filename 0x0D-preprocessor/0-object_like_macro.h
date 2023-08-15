b04a79d8ef565bce7b6bceb9c2c11bab3720355f
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-15 01:31:47 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,1 +1,7 @@
+// 0-object_like_macro.h
+#ifndef OBJECT_LIKE_MACRO_H   // This checks if the identifier is not defined
+#define OBJECT_LIKE_MACRO_H   // If not defined, define it now
+
 #define SIZE 1024
+
+#endif  // End of include guard
