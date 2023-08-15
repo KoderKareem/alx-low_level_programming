fe034e67ec71f9dee1057ef085e1ca38043a2582
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-15 09:12:50 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,19 @@
-#ifndef DOG_H
-#define DOG_H
-void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
