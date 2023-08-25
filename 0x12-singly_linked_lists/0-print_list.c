d4acde0a70772aba752793fedc6b34de8734e9ad
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-25 03:46:08 -0700

    added lists.h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-#include "main.h"
-
-typedef struct list_t {
-    char *str;
-    struct list_t *next;
-} list_t;
+#include <string.h>
+#include "lists.h"
 
 size_t print_list(const list_t *h) {
     size_t count = 0;
 
     while (h != NULL) {
         if (h->str != NULL) {
-            printf("%s\n", h->str);
+            printf("[%u] %s\n", h->len, h->str);
         } else {
             printf("[0] (nil)\n");
         }
--- 0x12-singly_linked_lists/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-size_t print_list(const list_t *h);
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-#endif
