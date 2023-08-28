6f9a1a58f328ac840ff9776c2e6dc8eec0a9e43d
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-28 03:21:00 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib>
+#include "lists.h"
+/**
+* list_len - print the element of the list
+* @h: return poiter to h
+* Return: (COUNT);
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
