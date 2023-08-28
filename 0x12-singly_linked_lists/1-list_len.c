1cd5702d60f876211da25dcfc27268b85cbee073
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-28 03:40:04 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,17 @@
 size_t list_len(const list_t *h)
 {
 	size_t count;
-
+	if (h == NULL)
+	{
+	return (0);
+	}
+	else
+	{
 	while (h != NULL)
 	{
 		h = h->next;
 		count++;
 	}
-
+	}
 	return (count);
 }
